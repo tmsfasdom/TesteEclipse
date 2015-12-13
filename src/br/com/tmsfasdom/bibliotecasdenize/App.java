3713283ae36df854d85186f3a114c16d8dfbcf58
Fernando <Fernando@192.168.171.11>
2015-12-13 10:33:16 -0200

    adicionada lib modificado codigo

--- src/br/com/tmsfasdom/bibliotecasdenize/App.java
+++ src/br/com/tmsfasdom/bibliotecasdenize/App.java
@@ -1,11 +1,14 @@
 package br.com.tmsfasdom.bibliotecasdenize;
 
+import com.google.gson.Gson;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("primeiro commit");
 System.out.println("segundo commit");
+System.out.println("terceiro commit");
 	}
 
 }
