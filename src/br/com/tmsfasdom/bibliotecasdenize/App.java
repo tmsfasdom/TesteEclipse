ec50662f9ed3f71c2bb826dfe73977437c976877
Denize <Denize@Note-Denize>
2015-12-13 10:26:08 -0200

    segundo commit do guaruja

--- src/br/com/tmsfasdom/bibliotecasdenize/App.java
+++ src/br/com/tmsfasdom/bibliotecasdenize/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("primeiro commit");
+System.out.println("segundo commit");
 	}
 
 }
